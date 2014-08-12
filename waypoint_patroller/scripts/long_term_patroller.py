fa4270e2e6d609cc02a064cb494f653766c64d3c
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-08-12 14:45:45 +0100

    Renamed ros_datacentre to mongodb_store
    This simply bulk replaces all ros_datacentre strings to mongodb_store strings inside files and also in file names.
    
    Needs strands-project/ros_datacentre#76 to me merged first.

--- waypoint_patroller/scripts/long_term_patroller.py
+++ waypoint_patroller/scripts/long_term_patroller.py
@@ -13,8 +13,8 @@ from waypoint_patroller.logger import PatrollLogger
 from dynamic_reconfigure.server import Server
 from waypoint_patroller.cfg import PatrollerTresholdsConfig
 
-import ros_datacentre.util
-got_pymongo = ros_datacentre.util.check_for_pymongo()
+import mongodb_store.util
+got_pymongo = mongodb_store.util.check_for_pymongo()
 if got_pymongo:
     import pymongo
     
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     rospy.init_node('patroller')
     
     # Check for connection to the datacentre
-    got_mongo = ros_datacentre.util.wait_for_mongo()
+    got_mongo = mongodb_store.util.wait_for_mongo()
     if not got_pymongo:
         sys.exit(1)
     if not got_mongo:
